94cb660614ef32df3501a344c200296ba70b1c18
HGB3009 <baobaokg9@gmail.com>
2024-12-03 10:00:35 +0700

    fix yarn.lock

--- src/controllers/regulation.controller.js
+++ src/controllers/regulation.controller.js
@@ -2,14 +2,12 @@
 const { getAnswerFromPython } = require('../services/regulation.service');
 
 const predictAnswer = async (req, res) => {
-    try {
-        const question = req.body.question;
-        const { answer, base } = await getAnswerFromPython(question);
-        res.json({ answer, base });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
+  try {
+    const { answer, base } = await getAnswerFromPython(req.body.question);
+    res.json({ answer, base });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 module.exports = { predictAnswer };
