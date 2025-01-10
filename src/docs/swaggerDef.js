839d48bf3d711d1a0795c94127716c27618bde7f
HGB3009 <baobaokg9@gmail.com>
2025-01-10 15:55:07 +0700

    Update swagger

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,5 +1,4 @@
 const { version } = require('../../package.json');
-const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
@@ -11,14 +10,6 @@ const swaggerDef = {
       url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
     },
   },
-  servers: [
-    {
-      url: `${config.host}/v1`,
-    },
-    {
-      url: `http://localhost:${config.port}/v1`,
-    },
-  ],
 };
 
 module.exports = swaggerDef;
