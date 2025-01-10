0e95632ce1fc5a85aa6b9d55f196e9a8c776d039
HGB3009 <baobaokg9@gmail.com>
2025-01-10 16:39:21 +0700

    update swagger

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,5 +1,4 @@
 const { version } = require('../../package.json');
-const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
@@ -13,10 +12,7 @@ const swaggerDef = {
   },
   servers: [
     {
-      url: `${config.host}/v1`,
-    },
-    {
-      url: `http://localhost:${config.port}/v1`,
+      url: '/',
     },
   ],
 };
